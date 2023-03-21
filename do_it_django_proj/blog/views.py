094b81e3abcfd97c444a2d0ad711035fb0beff6b
Sunbin Shin <202015046@smu.ac.kr>
2023-03-21 22:59:06 +0900

    DetailView로 포스트 상세 페이지를 만들었습니다.

--- do_it_django_proj/blog/views.py
+++ do_it_django_proj/blog/views.py
@@ -1,25 +1,9 @@
-from django.shortcuts import render
+from django.views.generic import ListView, DetailView
 from .models import Post
 
-def index(request):
-    #사용자의 요청을 받아서 여러가지 일을 처리..
-    posts = Post.objects.all().order_by('-pk')
+class PostList(ListView):
+    model = Post
+    ordering = '-pk'
 
-    return render(
-        request,
-        'blog/index.html',
-        {
-            'posts': posts,
-        }
-    )
-
-def single_post_page(request, pk):
-    post = Post.objects.get(pk=pk)
-
-    return render(
-        request,
-        'blog/single_post_page.html',
-        {
-            'post': post,
-        }
-    )
+class PostDetail(DetailView):
+    model = Post
