0ad9cbccdc39d7c25641e9539076e9a3843b63b1
Sunbin Shin <202015046@smu.ac.kr>
2023-04-04 16:16:28 +0900

    모델의 카테고리 추가

--- do_it_django_proj/blog/models.py
+++ do_it_django_proj/blog/models.py
@@ -1,6 +1,17 @@
 import os.path
 
 from django.db import models
+from django.contrib.auth.models import User
+
+class Category(models.Model):
+    name = models.CharField(max_length=50, unique=True)
+    slug = models.SlugField(max_length=200, unique=True, allow_unicode=True) #slug값을 기준으로 해당 카테고리의 글들을 볼 수 있다.
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name_plural = 'Categories'
 
 class Post(models.Model):
     title = models.CharField(max_length=30)
@@ -11,10 +22,14 @@ class Post(models.Model):
 
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
-    #author: 추후 작성 예정
+
+    author = models.ForeignKey(User, null=True, on_delete=models.SET_NULL)
+    #call back 함수
+    category = models.ForeignKey(Category, null=True, blank=True, on_delete=models.SET_NULL)
 
     def __str__(self):
-        return f'[{self.pk}]{self.title}'
+        return f'[{self.pk}]{self.title} :: {self.author}'
+        #연결되어 있는 User 삭제될때 models.CASCADE에 연결되어 있는 post 레코드까지 함께 삭제
 
     def get_absolute_url(self):
         return f'/blog/{self.pk}/'
