d91cc73c35c1724abfe80ec688ee03569617a118
Sunbin Shin <202015046@smu.ac.kr>
2023-04-18 17:32:55 +0900

    댓글 관련 및 코멘트 추가

--- do_it_django_proj/blog/views.py
+++ do_it_django_proj/blog/views.py
@@ -1,28 +1,34 @@
 from django.views.generic import ListView, DetailView, CreateView, UpdateView
+from .forms import CommentForm
 from .models import Post, Category, Tag
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin #로그인 방문자만 접근할 수 있도록 조치
 
 
 class PostUpdate(LoginRequiredMixin, UpdateView):
+
     model = Post
     fields = ['title', 'content', 'head_image', 'file_upload', 'category', 'tag']
     template_name = 'blog/post_update.html'
 
     def dispatch(self, request, *args, **kwargs):
+
         if request.user.is_authenticated and request.user == self.get_object().author:
             return super(PostUpdate, self).dispatch(request, *args, **kwargs)
         else:
             raise PermissionError
 
 class PostCreate(LoginRequiredMixin, UserPassesTestMixin, CreateView):
+
     model = Post
     fields = ['title', 'content', 'head_image', 'file_upload', 'category', 'tag']
 
     def test_func(self):
+
         return self.request.user.is_staff or self.request.user.is_superuser
 
     def form_valid(self, form):
+
         if self.request.user.is_authenticated and (self.request.user.is_superuser or self.request.user.is_staff):
             form.instance.author = self.request.user
             return super(PostCreate, self).form_valid(form)
@@ -30,6 +36,7 @@ class PostCreate(LoginRequiredMixin, UserPassesTestMixin, CreateView):
             return redirect('/blog/')
 
     def get_context_data(self, **kwargs):
+
         context = super(PostCreate, self).get_context_data()
         context['categories'] = Category.objects.all()
         context['no_category_count'] = Post.objects.filter(category=None).count()
@@ -40,21 +47,26 @@ class PostList(ListView):
     ordering = '-pk'
 
     def get_context_data(self, **kwargs):
+
         context = super(PostList, self).get_context_data()
         context['categories'] = Category.objects.all()
         context['no_category_count'] = Post.objects.filter(category=None).count()
+        context['comment_form'] = CommentForm
         return context
 
 class PostDetail(DetailView):
     model = Post
 
     def get_context_data(self, **kwargs):
+
         context = super(PostDetail, self).get_context_data()
         context['categories'] = Category.objects.all()
         context['no_category_count'] = Post.objects.filter(category=None).count()
+        context['comment_form'] = CommentForm
         return context
 
 def categories_page(request, slug):
+
     if slug=='no-category':
         category='미분류'
         post_list = Post.objects.filter(category=None)
@@ -83,3 +95,20 @@ def tag_page(request, slug):
         'no_category_count': Post.objects.filter(category=None).count()
     }
     return render(request, 'blog/post_list.html', context)
+
+def add_comment(request, pk):
+
+    if not request.user.is_authenticated:
+        raise PermissionError
+
+    if request.method == 'POST':
+        post = Post.objects.get(pk=pk)
+        comment_form = CommentForm(request.POST)
+        comment_temp = comment_form.save(commit=False)
+        comment_temp.post = post
+        comment_temp.author = request.user
+        comment_temp.save()
+        return redirect(post.get_absolute_url())
+
+    else:
+        raise PermissionError
--- do_it_django_proj/single_pages/views.py
+++ do_it_django_proj/single_pages/views.py
@@ -1,12 +1,19 @@
 from django.shortcuts import render
 
-def landing(request):
+from blog.models import Post
+
+
+def main(request):
     return render(
         request,
         'single_pages/main.html'
     )
-def main(request):
-    return render(request, 'single_pages/main.html')
+def landing(request):
+    recent_posts = Post.objects.order_by('-pk')[:3]
+    return render(request, 'single_pages/main.html',
+                  {
+                      'recent_posts': recent_posts
+                  })
 # Create your views here.
 def about_me(request):
     return render(
